c9d3e728d662fcf153fd5d18b555ae4dce06c83a
NV <nvardell@gmail.com>
2022-07-22 09:01:02 -0400

    Pushing latest notes.

--- Express/app.js
+++ Express/app.js
@@ -1,8 +1,10 @@
-const express = require('express')
+const express = require("express")
+const csvParser = require("csv-parser");
+
 const app = express()
 
-app.get('/', (req, res) => {
-  res.send('Hello World')
+app.get("/", (req, res) => {
+  res.send("Hello World")
 })
 
 app.listen(3000)
