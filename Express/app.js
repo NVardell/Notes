4c41b6776b6f4a36514ae582fc59abff0fcdb14f
NV <nvardell@gmail.com>
2022-07-22 09:02:10 -0400

    Pushing latest notes.

--- Express/app.js
+++ Express/app.js
@@ -3,6 +3,18 @@ const csvParser = require("csv-parser");
 
 const app = express()
 
+const result = [];
+
+fs.createReadStream("./data.csv")
+  .pipe(csvParser())
+  .on("data", (data) => {
+    result.push(data);
+  })
+  .on("end", () => {
+    console.log(result);
+  });
+
+
 app.get("/", (req, res) => {
   res.send("Hello World")
 })
