b3114b318b1c1b6ed2afd437057fde682ce79854
NV <nvardell@gmail.com>
2022-07-22 10:21:58 -0400

    Pushing latest notes.

--- Express/app.js
+++ Express/app.js
@@ -1,26 +1,14 @@
-const fs = require("fs");
-const express = require("express");
-const csvParser = require("csv-parser");
+const csv = require('csv-parser');  // https://github.com/mafintosh/csv-parser
+const fs = require('fs');
+const results = [];
 
-const app = express()
-
-const result = [];
-
-fs.createReadStream("data.csv")
-  .pipe(csvParser())
-  .on("data", (data) => {
-    result.push(data);
-  })
-  .on("end", () => {
-    console.log(result);
+fs.createReadStream('data.csv')
+  .pipe(csv(["Name", "Phone"]))
+  .on('data', (data) => results.push(data))
+  .on('end', () => {
+    console.log(results);
   });
-fs.writeFile("data2.csv", result, "utf-8", (err) => {
-  if (err) console.log(err);
-  else console.log("Data saved");
-});
-
-app.get("/", (req, res) => {
-  res.send("Hello World")
-})
-
-app.listen(3000)
+// fs.writeFile("data2.csv", results, "utf-8", (err) => {
+//   if (err) console.log(err);
+//   else console.log("Data saved");
+// });
