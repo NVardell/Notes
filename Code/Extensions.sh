914d5156d4e170eea203d42fe138c93bb06a4d59
NV <nvardell@gmail.com>
2022-08-20 18:53:22 -0400

    Pushing latest notes.

--- Code/Extensions.sh
+++ Code/Extensions.sh
@@ -31,6 +31,8 @@
             - Ex.
                 find . -not -name "*.*" | while read FILE; do mv "$FILE" "$FILE".jpg; done
 
+
+
 ##  
 ##  4. Discriminate by File Type
 ##  
@@ -45,10 +47,13 @@
                     3. 'readme'
                     4. 'nested_directory', and inside that directory, 
                     5.    \--- 'another_image'
-            - We want to rename only the 'image' and 'another_image' files, 
+            - We want to rename only the 'image' & 'another_image' files, 
               as the remaining files have an extension already or aren’t 
               really JPEG images.  Fortunately, the find command will also
-              list the nested files. 
+              list the nested files.
+##
+##      4a. Solve the problems individually
+##
             - If we want to filter out directories, we use the –type parameter.
                 + Ex.
                     find . -type f -not -name "*.*" ./image ./readme ./nested_directory/another_image
@@ -60,28 +65,36 @@
                     file --mime-type -b 'readme'
                     text/plain
                     ## 
-                    ## F
+                    ## Find & Print File Type
                     file --mime-type -b 'image-no-extension'
                     image/jpeg
                     ## 
-                    ## 
+                    ## Find & Print File Type
                     file --mime-type -b 'nested-directory'
                     inode/directory
-        + Finally, we’ll use a while loop with a conditional statement to iterate over the files.
-            + Ex.
+##
+##      4b. Solve problems simultaneously
+##
+        ## 
+        ## Option #1
+        ##
+        ##   Conditional while loop to iterate over the files.
+        ##
                 find . -type f -not -name "*.*" | while read FILE; 
                 do if [ $(file --mime-type -b "$FILE") == "image/jpeg" ]; 
                 then mv "$FILE" "$FILE".jpg; fi; done;
 
-
-We could also use the -exec parameter of the find command, but then, we’d need to execute our conditional statement with the bash or sh command:
+        We could also use the -exec parameter of the find command, but then, we’d need to execute our conditional statement with the bash or sh command:
 find . -type f -not -name "*.*" -exec bash -c 'if [ $(file --mime-type -b {}) == "image/jpeg" ]; then mv {} {}.jpg; fi;' \;
 If we’d like to perform a dry run to make sure our command will do what we intend to, we can simply substitute the mv command with the echo command to print old and new filenames.
 
-5. Summary
-In this tutorial, we learned how to use Bash to precisely add extensions to the extensionless files. We considered various possible pitfalls like files already having the extension, files of the wrong type, or nested directories.
 
 
-
+##
+##  5. Summary
+##
+In this tutorial, we learned how to use Bash to precisely add extensions to the extensionless files. 
+We considered various possible pitfalls like files already having the extension, 
+files of the wrong type, or nested directories.
 
 
